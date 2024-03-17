733600a42930d95fd2dbde32b1f47ddc4271031e
Mary <marieshix98@gmail.com>
2024-03-17 23:58:33 +0300

    update container max width

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 export const Container = ({ children }: Props) => {
   return (
-    <div className="max-w-[1820px] m-auto md:px-2 px-4 xl:px-20">
+    <div className="max-w-[1240px] m-auto md:px-2 px-4 xl:px-20">
       {children}
     </div>
   );
