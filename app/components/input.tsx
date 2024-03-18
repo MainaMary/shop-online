c6eca50951a5aac9e5ec09cd9cabbbf42061e945
Mary <marieshix98@gmail.com>
2024-03-18 14:32:18 +0300

    add props to the input element

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -4,14 +4,28 @@ interface InputProps {
   type?: string;
   id?: string;
   className?: string;
+  name: string;
+  onChange?: (x: any) => void;
+  value: string | number;
 }
 
-const Input = ({ placeholder, type, id, className = "" }: InputProps) => {
+const Input = ({
+  placeholder,
+  type,
+  name,
+  onChange,
+  id,
+  className = "",
+  value,
+}: InputProps) => {
   return (
     <input
       placeholder={placeholder}
       type={type}
       id={id}
+      value={value}
+      name={name}
+      onChange={onChange}
       className={`block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${className}`}
     />
   );
