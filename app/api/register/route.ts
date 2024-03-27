cb8435325eacbed01b3c044223c8fa054430a26c
Mary <marieshix98@gmail.com>
2024-03-27 16:25:19 +0300

    refactor aut endpoint

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,19 +2,26 @@ import client from "@/libs/prismadb";
 import bcrypt from "bcrypt";
 import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-async function POST(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { email, password, name } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const data = { email, name, hashedPassword };
-    const user = await client.user.create({
-      data,
-    });
-    return user;
-  } catch (error: any) {
-    console.log(error.message);
-  }
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, password } = body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const data = { email, name, hashedPassword };
+  const user = await client.user.create({ data });
+  return NextResponse.json(user);
 }
-
-export { POST };
+// async function POST(req: NextApiRequest, res: NextApiResponse) {
+//   try {
+//     const { email, password, name } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const data = { email, name, hashedPassword };
+//     const user = await client.user.create({
+//       data,
+//     });
+//     return user;
+//   } catch (error: any) {
+//     console.log(error.message);
+//   }
+// }
