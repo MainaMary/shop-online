a111929001ebe2756d28729e5509c607962db972
Mary <marieshix98@gmail.com>
2024-03-27 23:57:45 +0300

    map through side nav menu items

--- app/components/sidenav.tsx
+++ app/components/sidenav.tsx
@@ -2,22 +2,35 @@ import React from "react";
 const menuItems = [
   {
     label: "All",
+    id: "1",
   },
   {
     label: "Phone",
+    id: "2",
   },
   {
     label: "Laptop",
+    id: "3",
   },
   {
     label: "Desktop",
+    id: "4",
   },
   {
     label: "Watch",
+    id: "5",
   },
 ];
 const SideNav = () => {
-  return <div className="px-12">sidenav</div>;
+  return (
+    <div className="px-12">
+      {menuItems.map(({ label, id }) => (
+        <p className="my-4" key={id}>
+          {label}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default SideNav;
