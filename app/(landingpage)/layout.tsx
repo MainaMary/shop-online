e8ea936a4d036a7d4c97eadc8377c185ada90db7
Mary <marieshix98@gmail.com>
2024-03-27 20:19:19 +0300

    define side navigation bar reusable component

--- app/(landingpage)/layout.tsx
+++ app/(landingpage)/layout.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { Navbar } from "../components/navbar";
+import { SideNav } from "@/app/components";
 
 interface Props {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ interface Props {
 export default function LandingPageLayout({ children }: Props) {
   return (
     <div className="max-w-screen relative flex min-h-screen">
-      <div className="w-[15%]">Side Navbar</div>
+      <SideNav />
       <main className="max-w-screen flex-1">{children}</main>
     </div>
   );
---
+++ app/components/sidenav.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+const menuItems = [
+  {
+    label: "All",
+  },
+  {
+    label: "Phone",
+  },
+  {
+    label: "Laptop",
+  },
+  {
+    label: "Desktop",
+  },
+  {
+    label: "Watch",
+  },
+];
+const SideNav = () => {
+  return <div className="px-12">sidenav</div>;
+};
+
+export default SideNav;
