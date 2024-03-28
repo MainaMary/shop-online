fa8a9e765ff421efdab48b321ca66c6e8ea06de9
Mary <marieshix98@gmail.com>
2024-03-28 21:59:13 +0300

    add logic to add a product to cart

--- app/components/product-card.tsx
+++ app/components/product-card.tsx
@@ -1,19 +1,54 @@
 "use client";
+import { useState } from "react";
 import Rating from "@mui/material/Rating";
 import Image from "next/image";
 import { truncateText } from "../../utils/truncate";
 import { formatPrice } from "../../utils/format-price";
 import { productReview } from "@/utils/product-review";
-import Link from "next/link";
+import { CartProductType } from "@/types/types";
 import { useRouter } from "next/navigation";
-
+import { useCart } from "@/hooks/useCartContext";
+import Button from "./button";
+import ProductQuantity from "./product-quantity";
 interface ProductProps {
   product: any;
 }
 export function ProductCard({ product }: ProductProps) {
+  const [isProductInCart, setIsProductInCart] = useState(false);
+  const [cartItem, setCartItem] = useState<CartProductType>({
+    id: product ? product.id : "",
+    name: product ? product.name : "",
+    desc: product ? product.description : "",
+    category: product ? product.category : "",
+    brand: product ? product.brand : "",
+    image: product ? product.images[0].image : "",
+    selectedImg: product ? { ...product.selectedImg } : "",
+    quantity: 1,
+    price: product ? product.price : "",
+  });
   const router = useRouter();
+  const { addToCart } = useCart();
   const handleNavigate = () => {
     router.push(`/product/${product.id}`);
+  };
+  const handleAddToCart = (cartItem: CartProductType) => {
+    addToCart(cartItem);
+  };
+  const handleIncrease = () => {
+    setCartItem((prev) => {
+      return {
+        ...prev,
+        quantity: prev.quantity + 1,
+      };
+    });
+  };
+  const handleDecrease = () => {
+    setCartItem((prev) => {
+      return {
+        ...prev,
+        quantity: prev.quantity - 1,
+      };
+    });
   };
   return (
     <div className=" transition hover:scale-105 text-center text-sm col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-md p-2">
@@ -42,6 +77,19 @@ export function ProductCard({ product }: ProductProps) {
           <p className="font-semibold">{formatPrice(product.price)}</p>
         </div>
         <button>Add to cart</button>
+        {!isProductInCart ? (
+          <div>
+            <Button onClick={() => handleAddToCart(product)}>
+              Add to cart
+            </Button>
+          </div>
+        ) : (
+          <ProductQuantity
+            cartProduct={cartItem}
+            handleDecrease={handleDecrease}
+            handleIncrease={handleIncrease}
+          />
+        )}
       </div>
     </div>
   );
--- app/components/sidenav.tsx
+++ app/components/sidenav.tsx
@@ -1,31 +1,42 @@
 import React from "react";
+import { IoMdPhonePortrait } from "react-icons/io";
+import { FaLaptop } from "react-icons/fa";
+import { IoMdDesktop } from "react-icons/io";
+import { FiWatch } from "react-icons/fi";
+import { RiProductHuntFill } from "react-icons/ri";
 const menuItems = [
   {
     label: "All",
     id: "1",
+    icon: <RiProductHuntFill />,
   },
   {
     label: "Phone",
     id: "2",
+    icon: <IoMdPhonePortrait />,
   },
   {
     label: "Laptop",
     id: "3",
+    icon: <FaLaptop />,
   },
   {
     label: "Desktop",
     id: "4",
+    icon: <IoMdDesktop />,
   },
   {
     label: "Watch",
     id: "5",
+    icon: <FiWatch />,
   },
 ];
 const SideNav = () => {
   return (
     <div className="px-12">
-      {menuItems.map(({ label, id }) => (
-        <p className="my-4" key={id}>
+      {menuItems.map(({ label, id, icon }) => (
+        <p className="my-12 flex gap-2" key={id}>
+          {icon}
           {label}
         </p>
       ))}
