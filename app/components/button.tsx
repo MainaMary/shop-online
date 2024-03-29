f885ddba006248bb3b097d2e756e73c1c63c9cfc
Mary <marieshix98@gmail.com>
2024-03-29 22:19:42 +0300

    add more props to the button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,3 +1,4 @@
+import { ClassNames } from "@emotion/react";
 import React, { ReactNode } from "react";
 import { IconType } from "react-icons";
 interface BtnProps {
@@ -8,6 +9,7 @@ interface BtnProps {
   icon?: ReactNode;
   name?: string;
   type?: "submit" | "reset";
+  className?: string;
 }
 
 const Button = ({
@@ -18,15 +20,18 @@ const Button = ({
   disabled,
   icon,
   type,
+  className = "",
 }: BtnProps) => {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
       name={name}
+      type={type}
       className={`w-full border-slate-600 rounded-md text-center hover:opacity-80  transition flex m-auto items-center h-auto py-2 px-3 justify-center
       ${outline ? "bg-white" : "bg-rose-500"}
       ${outline ? "text-rose-500" : "text-white"}
+      ${className}
       `}
     >
       {icon && icon}
